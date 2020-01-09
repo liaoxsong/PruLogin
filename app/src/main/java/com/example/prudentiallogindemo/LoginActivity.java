3709d371fa65f44028ce07268eb003001ff8d2c1
liaosong <liao_song@outlook.com>
2020-01-09 14:08:27 +0800

    create login xml with resources

--- app/src/main/java/com/example/prudentiallogindemo/LoginActivity.java
+++ app/src/main/java/com/example/prudentiallogindemo/LoginActivity.java
@@ -2,13 +2,31 @@ package com.example.prudentiallogindemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText usernameEdit = findViewById(R.id.edit_username);
+
+        ObjectAnimator anim = ObjectAnimator.ofInt(usernameEdit, "backgroundColor", Color.TRANSPARENT, Color.BLUE,
+                Color.TRANSPARENT);
+        anim.setDuration(1500);
+        anim.setStartDelay(2000);
+        anim.setEvaluator(new ArgbEvaluator());
+//        anim.setRepeatMode(Animation.REVERSE);
+//        anim.setRepeatCount(Animation.INFINITE);
+      //  anim.start();
     }
 }
